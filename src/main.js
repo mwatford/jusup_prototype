a86b59358f618518a778787ccfa7d56509cbba9d
Peupty <melvillewatford@gmail.com>
2019-12-10 10:58:10 +0100

    importing capitalize method for String, importing and registering baseIcon component

--- src/main.js
+++ src/main.js
@@ -4,10 +4,14 @@ import VueRouter from "vue-router";
 import { routes } from "./router/router.js";
 import { store } from "./store/store.js";
 import VueChatScroll from "vue-chat-scroll";
+import "./scripts/capitalize";
+import baseIcon from "./components/baseIcon.vue";
 
 Vue.config.productionTip = false;
 
 window.eventBus = new Vue();
+
+Vue.component("base-icon", baseIcon);
 
 Vue.use(VueRouter);
 Vue.use(VueChatScroll);
