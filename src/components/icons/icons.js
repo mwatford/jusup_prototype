745e1086fabdb2a552a93cdcdc2d80c5234b6773
Peupty <melvillewatford@gmail.com>
2019-12-10 11:11:40 +0100

    removing fistBump, facePalm

--- src/components/icons/icons.js
+++ src/components/icons/icons.js
@@ -1,10 +1,8 @@
 import heart from "./heart.vue";
-import fistBump from "./fistBump.vue";
 import hand from "./hand.vue";
 import paw from "./paw.vue";
 import glasses from "./glasses.vue";
 import sad from "./sad.vue";
 import smile from "./smile.vue";
-import facePalm from "./facePalm.vue";
 
-export default { heart, fistBump, hand, paw, glasses, sad, smile, facePalm };
+export default { heart, hand, paw, glasses, sad, smile };
