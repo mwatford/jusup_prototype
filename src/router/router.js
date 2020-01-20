08656c5cd8cbe6755315667fffeb5e45e420874b
Peupty <melvillewatford@gmail.com>
2020-01-20 21:29:13 +0100

    added options route

--- src/router/router.js
+++ src/router/router.js
@@ -1,7 +1,9 @@
 import Main from "../views/Main.vue";
 import addGroup from "../views/Add-group.vue";
+import Options from "../views/Options.vue";
 
 export const routes = [
   { path: "/", component: Main, name: "main" },
+  { path: "/options", component: Options, name: "options" },
   { path: "/add-group", component: addGroup, name: "add-group" }
 ];
