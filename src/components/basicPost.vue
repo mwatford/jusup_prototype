03a089a28117e678c50a6d0a079efe4be13d405c
Peupty <melvillewatford@gmail.com>
2020-01-21 03:31:45 +0100

    adding markup, styles

--- src/components/basicPost.vue
+++ src/components/basicPost.vue
@@ -1,33 +1,69 @@
 <template>
   <article class="post">
-    <img :src="author.avatar" class="post__avatar" />
-    <p class="post__message">{{ post.message }}</p>
+    <div class="post__header row">
+      <img :src="author.avatar" class="post__avatar" />
+      <div class="details col">
+        <h3 class="details__author" :style="{ color: color }">
+          {{ post.author }}
+        </h3>
+        <p class="details__time">12 min.</p>
+      </div>
+      <base-icon
+        iconType="paw"
+        height="35"
+        width="35"
+        :color="color"
+      ></base-icon>
+    </div>
     <div class="post__content col">
-      <img :src="post.image" class="post__image" v-if="post.image" />
-      <div class="post__reactions row" :style="{ background: color + '44' }">
-        <div
-          class="reaction row wrap center"
-          v-for="(reaction, index) in post.reactions"
-          :key="index"
-          :style="{
-            color: reaction.liked
-              ? getReactionColor(reaction.name, groupName)
-              : '#222222'
-          }"
-        >
-          <base-icon
-            :iconType="reaction.name"
-            width="25"
-            height="25"
-            :iconColor="
-              reaction.liked
+      <p class="post__message">{{ post.message }}</p>
+      <picture class="post__image">
+        <img :src="post.image" v-if="post.image" />
+        <div class="post__reactions row" :style="{ background: color + '44' }">
+          <div
+            class="reaction row wrap center"
+            v-for="(reaction, index) in post.reactions"
+            :key="index"
+            :style="{
+              color: reaction.liked
                 ? getReactionColor(reaction.name, groupName)
                 : '#222222'
-            "
-            @click.native="react(reaction.name)"
-          />
-          <div>{{ reaction.amount }}</div>
+            }"
+          >
+            <base-icon
+              :iconType="reaction.name"
+              width="25"
+              height="25"
+              :iconColor="
+                reaction.liked
+                  ? getReactionColor(reaction.name, groupName)
+                  : '#222222'
+              "
+              @click.native="react(reaction.name)"
+            />
+            <div>{{ reaction.amount }}</div>
+          </div>
         </div>
+      </picture>
+    </div>
+    <div class="post__comments col">
+      <ul class="comments" v-if="showComments && post.comments.length">
+        <li v-for="(comment, index) in post.comments" :key="index" tag="div">
+          <div class="asd">{{ comment.text }}</div>
+        </li>
+      </ul>
+      <div
+        class="extend"
+        :style="{ background: color }"
+        @click="showComments = !showComments"
+      >
+        <base-icon
+          class="margin-auto"
+          iconType="caret"
+          color="#ffffff"
+          height="12"
+          width="12"
+        ></base-icon>
       </div>
     </div>
   </article>
@@ -40,7 +76,8 @@ export default {
   props: ["post", "color", "groupName"],
   data() {
     return {
-      liked: this.mapReactions()
+      liked: this.mapReactions(),
+      showComments: false
     };
   },
   computed: {
@@ -66,13 +103,6 @@ export default {
     react(name, color) {
       this.$store.commit("REACT", { name, id: this.post.id });
     }
-
-    // getReactionColor(reaction, group) {
-    //   return this.$store.state.groups
-    //     .getGroups()
-    //     .find(el => el.name === group)
-    //     .reactions.find(el => el.name === reaction).color;
-    // }
   }
 };
 </script>
@@ -80,7 +110,7 @@ export default {
 <style lang="scss" scoped>
 .post {
   display: grid;
-  grid-template-columns: 45px 1fr;
+  grid-template-columns: 45px 1fr 45px;
   place-items: center;
   flex-direction: row;
   margin-top: 20px;
@@ -89,8 +119,17 @@ export default {
   justify-content: center;
   width: 100%;
   min-width: 270px;
-  margin: 20px;
-  padding: 0 10px;
+  // padding: 0 10px;
+
+  &__header,
+  &__content {
+    width: 100%;
+    padding: 0 10px;
+  }
+
+  &__header {
+    grid-column: 1 / -1;
+  }
 
   &__avatar {
     width: 45px;
@@ -102,18 +141,23 @@ export default {
     place-self: start;
     grid-column: 1 / -1;
     width: 100%;
-    // margin: 12px 0 0 10px;
-  }
-
-  &__image {
-    width: 100%;
-    height: 350px;
-    object-fit: cover;
   }
 
   &__message {
-    place-self: center start;
-    margin-left: 10px;
+    margin-left: 55px;
+    margin-bottom: 20px;
+  }
+
+  &__image {
+    position: relative;
+    height: 350px;
+    // overflow-x: scroll;
+
+    & img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
   }
 
   &__reactions {
@@ -124,6 +168,28 @@ export default {
     padding: 0 10px;
     // background: #ededed;
   }
+
+  &__comments {
+    grid-column: 1 / -1;
+    width: 100%;
+  }
+}
+
+.details {
+  width: 100%;
+  margin-left: 10px;
+  margin-bottom: 20px;
+
+  &__author {
+    width: 100%;
+    place-self: center start;
+    font-weight: bold;
+    font-size: 24px;
+  }
+
+  &__time {
+    font-size: 12px;
+  }
 }
 .reaction {
   width: 55px;
@@ -131,5 +197,11 @@ export default {
   svg {
     margin-right: 5px;
   }
+}
+.extend {
+  cursor: pointer;
+  display: flex;
+  width: 100%;
+  height: 18px;
 }
 </style>
