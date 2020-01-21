ace3a7db17930fec7a3e847444d66e129c5360f3
Peupty <melvillewatford@gmail.com>
2020-01-21 03:32:18 +0100

    added markup and css

--- src/views/Navigation.vue
+++ src/views/Navigation.vue
@@ -1,11 +1,99 @@
 <template>
   <div class="options row">
-    <div class="content"></div>
+    <div class="content col">
+      <div class="col">
+        <div class="box box-1 row">
+          <div class="content content--min">
+            <p>You are here</p>
+          </div>
+          <div class="navigation" style="font-size: 6px; color: #fff;"></div>
+        </div>
+        <div class="row">
+          <div class="box">
+            <div class="box__item box__item--high"></div>
+            <div class="box__item box__item--high"></div>
+            <div class="box__item box__item--high"></div>
+          </div>
+          <div class="box relative">
+            <div class="box__item box__item--high"></div>
+            <div class="box__item box__item--long"></div>
+            <div class="box__item "></div>
+            <div class="box__item "></div>
+            <base-icon
+              iconType="caret"
+              color="#e75995"
+              class="arrow arrow--left"
+              height="45"
+              width="45"
+            ></base-icon>
+            <base-icon
+              iconType="caret"
+              color="#e75995"
+              class="arrow arrow--down"
+              height="45"
+              width="45"
+            ></base-icon>
+            <base-icon
+              iconType="caret"
+              color="#e75995"
+              class="arrow arrow--right"
+              height="45"
+              width="45"
+            ></base-icon>
+          </div>
+          <div class="box">
+            <div class="box__item"></div>
+            <div class="box__item"></div>
+            <div class="box__item"></div>
+            <div class="box__item"></div>
+            <div class="box__item"></div>
+            <div class="box__item"></div>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="content">
+          <div class="col">
+            <base-icon
+              iconType="caret"
+              class="key key--up"
+              height="45"
+              width="45"
+            ></base-icon>
+            <div class="row">
+              <base-icon
+                iconType="caret"
+                class="key key--left"
+                height="45"
+                width="45"
+              ></base-icon>
+              <base-icon
+                iconType="caret"
+                class="key key--down"
+                height="45"
+                width="45"
+              ></base-icon>
+              <base-icon
+                iconType="caret"
+                class="key key--right"
+                height="45"
+                width="45"
+              ></base-icon>
+            </div>
+          </div>
+          <h3 class="tip">
+            Move between views using arrows on your keyboard
+          </h3>
+        </div>
+      </div>
+    </div>
     <nav class="navigation">
       <ul class="col">
-        <router-link to="/add-group">
-          <button>Add Group</button>
-        </router-link>
+        <button>
+          <router-link to="/add-group">
+            Add group
+          </router-link>
+        </button>
         <button>Navigation</button>
         <button>Edit desk</button>
         <button>Settings</button>
@@ -32,7 +120,17 @@ export default {
   width: 100%;
 }
 .content {
+  display: flex;
   width: 100%;
+  height: 100%;
+  color: #509df6;
+  justify-content: space-evenly;
+  align-items: center;
+
+  &--min {
+    justify-content: center;
+    align-items: center;
+  }
 }
 .navigation {
   position: relative;
@@ -52,5 +150,88 @@ button {
   border-radius: 4px;
   font-size: 16px;
   margin: 8px 0;
+}
+.wrapper {
+  display: grid;
+  grid-template-columns: 260px 70px 260px 70px 260px;
+  grid-template-rows: 140px 140px;
+}
+.box {
+  width: 260px;
+  height: 140px;
+  border: 2px solid #e75995;
+  margin: 50px;
+  padding: 4px;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(2, 1fr);
+  grid-gap: 4px;
+
+  &-1 {
+    margin: 50px auto;
+    display: flex;
+    padding: 0;
+  }
+
+  &-3 {
+    position: relative;
+  }
+
+  &__item {
+    height: 100%;
+    width: 100%;
+    border: 1px solid #e75995;
+    border-radius: 5px;
+
+    &--high {
+      grid-row: span 2;
+    }
+    &--long {
+      grid-column: span 2;
+    }
+  }
+}
+.arrow {
+  position: absolute;
+
+  &--left {
+    margin-right: 30px;
+    right: 100%;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+  &--right {
+    margin-left: 30px;
+    left: 100%;
+    top: 50%;
+    transform: translateY(-50%) rotate(180deg);
+  }
+  &--down {
+    margin-bottom: 30px;
+    left: 50%;
+    bottom: 100%;
+    transform: translateX(-50%) rotate(-90deg);
+  }
+}
+.key {
+  color: #8c8c8c;
+  border: 2px solid #8c8c8c;
+  border-radius: 4px;
+  padding: 10px;
+  margin: 10px;
+
+  &--up {
+    margin: 10px auto;
+    transform: rotate(90deg);
+  }
+  &--right {
+    transform: rotate(180deg);
+  }
+  &--down {
+    transform: rotate(-90deg);
+  }
+}
+.tip {
+  margin-left: 40px;
 }
 </style>
